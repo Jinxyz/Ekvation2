0688186b7c10bc8ccbe2a73b748d3a26aaf4eeec
Jinxyz <xxyaymanyayxx@hotmail.com>
2019-10-25 11:17:41 +0200

    torsdag

--- src/Ekvation.java
+++ src/Ekvation.java
@@ -3,30 +3,34 @@ import javax.swing.*;
 
 public class Ekvation {
     public static void main(String[] args) {
-
         {
-        int ekvation = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad"));
-        int a;
-        int b;
-        int c;
-        int potens;
-    } 
 
-        if (ekvation =0) {
-            a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+            int ekvation = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad"));
+            int a;
+            int b;
+            int c;
+            int potens;
+
+
+            if (ekvation == 0) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+                JOptionPane.showMessageDialog(null, a);
+
+
+            } else if (ekvation == 1) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "b?"));
+                JOptionPane.showMessageDialog(null, -a / b);
+
+
+            } else if (ekvation == 2) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "b?"));
+                c = Integer.parseInt(JOptionPane.showInputDialog(null, "c?"));
+                potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Potens?"));
+                JOptionPane.showMessageDialog(null, -b );
+            }
+
         }
-        else if (ekvation = 1) {
-           a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
-           b = Integer.parseInt(JOptionPane.showInputDialog(null, "b?"));
-
-        }
-
-        else if (ekvation = 2){
-            a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
-            b = Integer.parseInt(JOptionPane.showInputDialog(null, "b?"));
-            c = Integer.parseInt(JOptionPane.showInputDialog(null, "c?"));
-            potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Potens?"));
-        }
-
     }
 }
